87e150548c474322f3ddc96e539d93cc33432612
Mariusz <mariusz.lepkowski23@gmail.com>
2024-01-10 16:55:24 +0100

    Initialize 'random_album_generator_app' with an empty structure.

---
+++ DrumLib/random_album_generator_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RandomAlbumGeneratorAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'random_album_generator_app'
