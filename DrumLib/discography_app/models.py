4a0cc34f35433374d40c20d0cb0816ce51cf1df5
Mariusz <mariusz.lepkowski23@gmail.com>
2024-01-22 19:58:21 +0100

    Create a Track model representing a music track.

--- DrumLib/discography_app/models.py
+++ DrumLib/discography_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from drummers_app.models import Drummer
 
-# Create your models here.
+
+class Track(models.Model):
+    """
+    Model representing a music track.
+
+    Attributes:
+    - title: Track title.
+    - artist: Foreign key relationship with Artist model, indicating the artist or band associated with the track.
+    - drummers: Many-to-many relationship with Drummer model, stores drummers associated with the track.
+    """
+    title = models.CharField(max_length=255)
+    artist = models.ForeignKey(Artist, on_delete=models.SET_NULL, null=True, blank=True, related_name='tracks')
+    drummers = models.ManyToManyField(Drummer, related_name='tracks_played', blank=True)
+
+    def __str__(self):
+        return self.title
