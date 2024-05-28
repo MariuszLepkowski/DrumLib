48948ff148b45aa8dfcbbfa6ddc1f27c2cb1d0ae
Mariusz <mariusz.lepkowski23@gmail.com>
2024-05-28 11:07:41 +0200

    Configure URLs for drummers_app views. Fix loading assets inside drummers_app

--- DrumLib/drummers_app/urls.py
+++ DrumLib/drummers_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "drummers_app"
 
 urlpatterns = [
-    path("drummers/", views.drummers, name="drummers"),
+    path("drummers/", views.drummers, name="drummers_list"),
     path("drummers/<drummer_name>/", views.drummer_profile, name="drummer_profile"),
     path("drummers/add-drummer", views.add_drummer, name="drummers"),
 ]
--- DrumLib/drummers_app/views.py
+++ DrumLib/drummers_app/views.py
@@ -2,18 +2,30 @@ from django.shortcuts import render, HttpResponse
 from .models import Drummer, DrummerPhoto
 
 
+
 def drummers(request):
-    response = ('drummers view. '
-                'Displays list of drummers in the database with filtering and sorting options. '
-                'Each drummer has a profile with basic information and an associated discography.')
-    return HttpResponse(response)
+    # response = ('drummers view. '
+    #             'Displays list of drummers in the database with filtering and sorting options. '
+    #             'Each drummer has a profile with basic information and an associated discography.')
+    # return HttpResponse(response)
+
+    template_name = "drummers_app/drummers.html"
+
+    return render(request, template_name)
+
 
 
 def drummer_profile(request, drummer_name):
-    response = f'drummer_profile view. Displays {drummer_name} profile with basic information and an associated discography.'
-    return HttpResponse(response, drummer_name)
+    # response = f'drummer_profile view. Displays {drummer_name} profile with basic information and an associated discography.'
+    # return HttpResponse(response, drummer_name)
+    template_name = "drummers_app/drummer-profile.html"
+
+    return render(request, template_name)
 
 
 def add_drummer(request):
-    response = 'add_drummer view. Allows users to add_new drummer to db through form.'
-    return HttpResponse(response)
+    # response = 'add_drummer view. Allows users to add_new drummer to db through form.'
+    # return HttpResponse(response)
+    template_name = "drummers_app/add-drummer.html"
+
+    return render(request, template_name)
