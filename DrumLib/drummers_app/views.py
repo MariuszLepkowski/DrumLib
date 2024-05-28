4532cd6623345ec0f7a8366b4030eee4265bc26f
Mariusz <mariusz.lepkowski23@gmail.com>
2024-05-28 15:12:31 +0200

    Create badges for section titles.

--- DrumLib/drummers_app/views.py
+++ DrumLib/drummers_app/views.py
@@ -10,8 +10,11 @@ def drummers(request):
     # return HttpResponse(response)
 
     template_name = "drummers_app/drummers.html"
+    context = {
+        'title': 'Drummers Explorer'
+    }
 
-    return render(request, template_name)
+    return render(request, template_name, context)
 
 
 
@@ -19,13 +22,19 @@ def drummer_profile(request, drummer_name):
     # response = f'drummer_profile view. Displays {drummer_name} profile with basic information and an associated discography.'
     # return HttpResponse(response, drummer_name)
     template_name = "drummers_app/drummer-profile.html"
+    context = {
+        'title': f'{drummer_name} Profile'
+    }
 
-    return render(request, template_name)
+    return render(request, template_name, context)
 
 
 def add_drummer(request):
     # response = 'add_drummer view. Allows users to add_new drummer to db through form.'
     # return HttpResponse(response)
     template_name = "drummers_app/add-drummer.html"
+    context = {
+        'title': 'Add Drummer'
+    }
 
-    return render(request, template_name)
+    return render(request, template_name, context)
