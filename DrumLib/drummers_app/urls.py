1f8767f442eee9573d2338ae58cb02e221b7ecd8
Mariusz <mariusz.lepkowski23@gmail.com>
2024-06-02 13:57:00 +0200

    Create Add Drummer element inside navbar. Change text format inside features section

--- DrumLib/drummers_app/urls.py
+++ DrumLib/drummers_app/urls.py
@@ -7,5 +7,5 @@ app_name = "drummers_app"
 urlpatterns = [
     path("", views.drummers, name="drummers_list"),
     path("<drummer_name>/", views.drummer_profile, name="drummer_profile"),
-    path("add-drummer", views.add_drummer, name="drummers"),
+    path("add-drummer", views.add_drummer, name="add_drummer"),
 ]
