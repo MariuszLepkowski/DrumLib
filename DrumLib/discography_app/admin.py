b811a50ae57078963b364e4254fd3c1d546a3bb8
Mariusz <mariusz.lepkowski23@gmail.com>
2024-06-03 13:25:34 +0200

    Register drummers_app and discography_app models inside admin.py to allow controlling them inside admin panel

--- DrumLib/discography_app/admin.py
+++ DrumLib/discography_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Album, Artist, Track
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Album, AuthorAdmin)
+admin.site.register(Artist, AuthorAdmin)
+admin.site.register(Track, AuthorAdmin)
--- DrumLib/drummers_app/admin.py
+++ DrumLib/drummers_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Drummer, DrummerPhoto
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Drummer, AuthorAdmin)
+admin.site.register(DrummerPhoto, AuthorAdmin)
