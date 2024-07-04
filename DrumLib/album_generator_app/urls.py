ab7a62dd59e43185639ad4f597a8d043d3a20da5
Mariusz <mariusz.lepkowski23@gmail.com>
2024-07-04 22:58:29 +0200

    Create album_generator_form and generate_random_album views inside album_generator_app. Create basic template view for displaying form and generating results

--- DrumLib/album_generator_app/urls.py
+++ DrumLib/album_generator_app/urls.py
@@ -1,9 +1,19 @@
+# from django.urls import path
+# from . import views
+#
+# app_name = 'album_generator_app'
+
+
+# urlpatterns = [
+#     path('', views.random_album, name='random_album'),
+# ]
+
 from django.urls import path
-from . import views
+from .views import album_generator_form, generate_random_album
 
 app_name = 'album_generator_app'
 
-
 urlpatterns = [
-    path('', views.random_album, name='random_album'),
+    path('', album_generator_form, name='album_generator_form'),
+    path('generate/', generate_random_album, name='generate_random_album'),
 ]
--- DrumLib/album_generator_app/views.py
+++ DrumLib/album_generator_app/views.py
@@ -1,21 +1,74 @@
+# from django.shortcuts import render
+# from random import choice
+# from drummers_app.models import Drummer
+# from discography_app.models import Album
+#
+#
+# def random_album(request):
+#     drummers = Drummer.objects.all()
+#     random_drummer = choice(drummers)
+#     albums = Album.objects.filter(drummers=random_drummer).prefetch_related('artists')
+#     random_album = choice(albums) if albums.exists() else None
+#
+#     if random_album:
+#         artists = ', '.join(artist.name for artist in random_album.artists.all())
+#     else:
+#         artists = None
+#
+#     context = {
+#         'title': 'Album Generator',
+#         'drummer': random_drummer.name,
+#         'album': random_album.title if random_album else 'No album found',
+#         'artists': artists,
+#     }
+#
+#     return render(request, 'album_generator_app/album-generator.html', context)
+#
+#
+
+
 from django.shortcuts import render
 from random import choice
 from drummers_app.models import Drummer
 from discography_app.models import Album
+from discography_app.utils import get_video_id
 
 
-def random_album(request):
+def album_generator_form(request):
     drummers = Drummer.objects.all()
-    random_drummer = choice(drummers)
+    context = {
+        'title': 'Album Generator',
+        'drummers': drummers,
+    }
+    return render(request, 'album_generator_app/album-generator-form.html', context)
+
+
+def generate_random_album(request):
+    drummer_id = request.POST.get('drummer')
+    random_drummer = Drummer.objects.get(id=drummer_id)
     albums = Album.objects.filter(drummers=random_drummer)
     album = choice(albums) if albums.exists() else None
 
+    if album and album.tracks.exists():
+        tracks = album.tracks.all()
+
+        for track in tracks:
+            track.video_id = get_video_id(track.track_url)
+
+    else:
+        tracks = None
+
+    if album and album.artists.exists():
+        artists = ', '.join(artist.name for artist in album.artists.all())
+    else:
+        artists = None
+
     context = {
-        'title': 'Album Generator',
-        'drummer': random_drummer.name,
-        'album': album.title if album else 'No album found',
+        'title': 'Generated album',
+        'drummer': random_drummer,
+        'album': album,
+        'artists': artists,
+        'tracks': tracks,
     }
 
-    return render(request, 'album_generator_app/album-generator.html', context)
-
-
+    return render(request, 'album_generator_app/album-details.html', context)
