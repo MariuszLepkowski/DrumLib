2e059443f35d64df3d97da57ec1d87bb490fcba7
Mariusz <mariusz.lepkowski23@gmail.com>
2024-07-12 21:56:24 +0200

    Set up basic structure for logging users

--- DrumLib/user_management_app/urls.py
+++ DrumLib/user_management_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import register
+from django.contrib.auth import views as auth_views
 
 app_name = 'user_management_app'
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('login/',auth_views.LoginView.as_view(template_name='user_management_app/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(),name='logout'),
 ]
