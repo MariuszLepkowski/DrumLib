859084557ce6241c2fad511e8082f4b6e183b26d
Mariusz <mariusz.lepkowski23@gmail.com>
2024-07-24 17:01:34 +0200

    Change url paths for password reset views to display custom templates

--- DrumLib/user_management_app/urls.py
+++ DrumLib/user_management_app/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('profile/', profile, name='profile'),
 
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='user_management_app/password_reset.html'), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='user_management_app/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='user_management_app/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='user_management_app/password_reset_complete.html'), name='password_reset_complete'),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name='user_management_app/password_reset.html'), name='password_reset'),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='user_management_app/password_reset_done.html'), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='user_management_app/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='user_management_app/password_reset_complete.html'), name='password_reset_complete'),
 ]
