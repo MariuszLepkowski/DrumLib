7f8653978ffbf0b72f8ed2e604720b2f64c16efc
Mariusz <mariusz.lepkowski23@gmail.com>
2024-08-04 21:59:12 +0200

    Add Comment model and initial migration

--- DrumLib/comments_app/models.py
+++ DrumLib/comments_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from drummers_app.models import Drummer
+from discography_app.models import Album, Track
 
-# Create your models here.
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    drummer = models.ForeignKey(Drummer, on_delete=models.CASCADE, null=True, blank=True)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, null=True, blank=True)
+    track = models.ForeignKey(Track, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.created_at}'
+
