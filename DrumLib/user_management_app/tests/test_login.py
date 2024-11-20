073d45495f98c0ba0ad11e51e53348b22acf4ca7
Mariusz <mariusz.lepkowski23@gmail.com>
2024-11-20 16:50:33 +0100

    Add tests for CustomAuthenticationForm to validate login functionality.

---
+++ DrumLib/user_management_app/tests/test_login.py
@@ -0,0 +1,36 @@
+import pytest
+from django.contrib.auth.models import User
+from user_management_app.forms import CustomAuthenticationForm
+
+
+@pytest.mark.django_db
+class TestCustomAuthenticationForm:
+
+    @pytest.mark.login_tests
+    @pytest.mark.parametrize(
+        "username, password, expected_error",
+        [
+            ("existing_user", "correct_password", None),
+            ("nonexistent_user", "any_password", "The user does not exist."),
+            ("existing_user", "incorrect_password", "Invalid password. Try again."),
+
+        ],
+        ids=[
+            "valid_login",
+            "nonexistent_user",
+            "incorrect_password",
+        ]
+    )
+    def test_authentication_form(self, username, password, expected_error):
+        User.objects.create_user(username="existing_user", password="correct_password")
+
+        form_data = {"username": username, "password": password}
+
+        form = CustomAuthenticationForm(data=form_data)
+
+        if expected_error:
+            assert not form.is_valid()
+            assert expected_error in form.errors["__all__"][0]
+        else:
+            assert form.is_valid()
+
