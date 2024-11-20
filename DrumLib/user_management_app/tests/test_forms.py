d41c8332434426a56189a955fc385afc09222616
Mariusz <mariusz.lepkowski23@gmail.com>
2024-11-20 13:55:34 +0100

    Add parameterized tests for UserRegistrationForm in user_management_app
    - Introduced a comprehensive set of test cases using @pytest.mark.parametrize.
    - Covered scenarios for valid registration, mismatched passwords, missing email, and invalid email format.
    - Verified that valid data results in user creation, while invalid data is rejected by form validation.

--- DrumLib/user_management_app/tests/test_forms.py
+++ DrumLib/user_management_app/tests/test_forms.py
@@ -2,9 +2,70 @@ import pytest
 from django.contrib.auth.models import User
 from user_management_app.forms import UserRegistrationForm
 
+test_data = [
+    (  #valid_registration
+        {
+            "first_name": "John",
+            "last_name": "Doe",
+            "username": "test_user",
+            "email": "testemail@example.com",
+            "password1": "testpassword123",
+            "password2": "testpassword123",
+        },
+        True
+    ),
+    (  #invalid_passwords_mismatch
+        {
+            "first_name": "John",
+            "last_name": "Doe",
+            "username": "test_user",
+            "email": "testemail@example.com",
+            "password1": "testpassword123",
+            "password2": "differentpassword123",
+        },
+        False
+    ),
+    (  #missing_email
+        {   "first_name": "John",
+            "last_name": "Doe",
+            "username": "test_user",
+            "password1": "strongpassword123",
+            "password2": "strongpassword123",
+        },
+        False
+    ),
+    (  # wrong mail format
+        {   "first_name": "John",
+            "last_name": "Doe",
+            "username": "test_user",
+            "email": "mail.com",
+            "password1": "strongpassword123",
+            "password2": "strongpassword123",
+         },
+        False
+    )
+]
 
+
+@pytest.mark.django_db
 class TestRegistration:
 
     @pytest.mark.registration_tests
-    def test_user_registration_form(self):
-        pass
+    @pytest.mark.parametrize(
+        "data, is_valid",
+        test_data,
+        ids=[
+        "valid_registration",
+        "invalid_passwords_mismatch",
+        "missing_email",
+        "wrong_mail_format",
+        ]
+    )
+    def test_user_registration_form(self, data, is_valid):
+        form = UserRegistrationForm(data=data)
+
+        assert form.is_valid() == is_valid
+
+        if form.is_valid():
+            form.save()
+            assert User.objects.filter(username="test_user").exists()
