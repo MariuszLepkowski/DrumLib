87b7d29f413e0814d02301f93bd9caa46d763389
Mariusz <mariusz.lepkowski23@gmail.com>
2024-11-26 10:32:19 +0100

    Add fixtures for user and profile in conftest.py

---
+++ DrumLib/user_management_app/tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from django.contrib.auth.models import User
+from user_management_app.models import Profile
+
+@pytest.fixture
+def user():
+    """
+    Fixture creating a new User object.
+    """
+    return User.objects.create_user(
+        username="testuser",
+        password="password123",
+        first_name="John",
+        last_name="Doe",
+        email="testuser@example.com",
+    )
+
+@pytest.fixture
+def profile(user):
+    """
+    Fixture that retrieves the profile associated with the user.
+    It is assumed that the profile is automatically created by a signal.
+    """
+    return Profile.objects.get(user=user)
+
