3be89190d51fa59772dd55b96659ab037341d832
Mariusz <mariusz.lepkowski23@gmail.com>
2024-12-13 15:58:55 +0100

    test_drummers_view_uses_correct_template

--- DrumLib/drummers_app/tests/test_drummers_view.py
+++ DrumLib/drummers_app/tests/test_drummers_view.py
@@ -1,5 +1,3 @@
-from pydoc import resolve
-
 import pytest
 from drummers_app.views import drummers
 from django.urls import reverse, resolve
@@ -11,3 +9,9 @@ class TestDrummersView:
     def test_drummers_view_resolves_to_correct_view(self):
         url = reverse(viewname="drummers_app:drummers_list")
         assert resolve(url).func == drummers
+
+    def test_drummers_view_uses_correct_template(self, client):
+        url = reverse(viewname="drummers_app:drummers_list")
+        response = client.get(url)
+        assert 'drummers_app/drummers.html' in [t.name for t in response.templates]
+
