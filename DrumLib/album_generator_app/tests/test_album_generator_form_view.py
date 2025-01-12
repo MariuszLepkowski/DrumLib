8fd3569b65f98f324dbd252127fc5057ecd1e77b
Mariusz <mariusz.lepkowski23@gmail.com>
2025-01-12 15:06:38 +0100

    Add test_album_generator_form_view_uses_correct_template to test_album_generator_form_view.py

--- DrumLib/album_generator_app/tests/test_album_generator_form_view.py
+++ DrumLib/album_generator_app/tests/test_album_generator_form_view.py
@@ -3,7 +3,13 @@ from django.urls import reverse, resolve
 from album_generator_app.views import album_generator_form
 
 
+@pytest.mark.django_db
 class TestAlbumGeneratorFormView:
     def test_album_generator_form_view_resolves_to_correct_view(self):
         url = reverse(viewname="album_generator_app:album_generator_form")
         assert resolve(url).func == album_generator_form
+
+    def test_album_generator_form_view_uses_correct_template(self, client):
+        url = reverse(viewname="album_generator_app:album_generator_form")
+        response = client.get(url)
+        assert 'album_generator_app/album-generator-form.html' in [t.name for t in response.templates]
