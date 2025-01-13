3e25c00a21655fa729423d82871a29a6a565b4b1
Mariusz <mariusz.lepkowski23@gmail.com>
2025-01-13 12:15:01 +0100

    Add test for Album Generator Form view to verify drummers list in context

--- DrumLib/album_generator_app/tests/test_album_generator_form_view.py
+++ DrumLib/album_generator_app/tests/test_album_generator_form_view.py
@@ -1,6 +1,7 @@
 import pytest
 from django.urls import reverse, resolve
 from album_generator_app.views import album_generator_form
+from drummers_app.models import Drummer
 
 
 @pytest.mark.django_db
@@ -9,7 +10,26 @@ class TestAlbumGeneratorFormView:
         url = reverse(viewname="album_generator_app:album_generator_form")
         assert resolve(url).func == album_generator_form
 
+    def test_album_generator_form_view_status_code(self, client):
+        url = reverse(viewname="album_generator_app:album_generator_form")
+        response = client.get(url)
+        assert response.status_code == 200
+
     def test_album_generator_form_view_uses_correct_template(self, client):
         url = reverse(viewname="album_generator_app:album_generator_form")
         response = client.get(url)
         assert 'album_generator_app/album-generator-form.html' in [t.name for t in response.templates]
+
+    def test_album_generator_form_view_context_has_drummer_list(self, client):
+        url = reverse(viewname="album_generator_app:album_generator_form")
+
+        Drummer.objects.create(name="John Bonham", bio="Legendary drummer of Led Zeppelin.")
+        Drummer.objects.create(name="Neil Peart", bio="Iconic drummer of Rush.")
+
+        response = client.get(url)
+        assert 'drummers' in response.context
+
+        drummers = response.context['drummers']
+        assert drummers.count() == 2
+        assert any(d.name == "John Bonham" for d in drummers)
+        assert any(d.name == "Neil Peart" for d in drummers)
