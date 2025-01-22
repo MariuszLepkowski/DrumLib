aed991bc1b72f10b0f6b5b614576deefaaa15e0c
Mariusz <mariusz.lepkowski23@gmail.com>
2025-01-22 14:22:59 +0100

    Add tests for get_video_id utility function in discography_app.utils

---
+++ DrumLib/discography_app/tests/test_discography_utils.py
@@ -0,0 +1,15 @@
+import pytest
+from discography_app.utils import get_video_id
+
+@pytest.mark.parametrize(
+    "url, expected_video_id",
+    [
+        ("https://www.youtube.com/watch?v=abc123", "abc123"),
+        ("https://youtu.be/abc123?v=xyz456", "xyz456"),
+        ("https://www.youtube.com/watch?v=def456&feature=youtu.be", "def456"),
+        ("https://www.youtube.com/watch", None),
+        ("invalid-url", None),
+    ],
+)
+def test_get_video_id(url, expected_video_id):
+    assert get_video_id(url) == expected_video_id
