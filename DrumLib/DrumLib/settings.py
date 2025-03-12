d41c427c7db9d1ac0acdd7f84ff0ff05cd0d3354
Mariusz <mariusz.lepkowski23@gmail.com>
2025-03-12 20:41:59 +0100

    settings.py for DEPLOY ON RENDER.COM

--- DrumLib/DrumLib/settings.py
+++ DrumLib/DrumLib/settings.py
@@ -27,12 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
     'localhost',
-    '127.0.0.1'
+    '127.0.0.1',
+    'drumlib-render-latest.onrender.com',
 ]
 
 
@@ -94,17 +95,27 @@ WSGI_APPLICATION = 'DrumLib.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         "NAME": os.environ.get('DB_NAME'),
+#         "USER": os.environ.get('DB_USER'),
+#         "PASSWORD": os.environ.get('DB_PASSWORD'),
+#         "HOST": "db",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        "NAME": os.environ.get('DB_NAME'),
-        "USER": os.environ.get('DB_USER'),
-        "PASSWORD": os.environ.get('DB_PASSWORD'),
-        "HOST": "db",
+        "NAME": os.environ.get('PROD_DB_NAME'),
+        "USER": os.environ.get('PROD_DB_USER'),
+        "PASSWORD": os.environ.get('PROD_DB_PASSWORD'),
+        "HOST": os.environ.get('PROD_DB_HOST'),
         "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
