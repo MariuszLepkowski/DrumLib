6ac0afaeae93fa8b826955825bc1510e80dc6ddb
Mariusz <mariusz.lepkowski23@gmail.com>
2025-09-07 14:15:56 +0200

    refactor(home): replace function-based home_page view with CBV
    Migrated the home_page function-based view to a class-based TemplateView.

--- DrumLib/home/views.py
+++ DrumLib/home/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def home_page(request):
+class HomeView(TemplateView):
     template_name = "home/home-page.html"
-    return render(request, template_name)
